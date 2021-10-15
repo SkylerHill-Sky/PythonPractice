7c52e2da4560165716d5dff14966b6fed8671f8c
Skyler Hill <skylerhil@me.com>
2021-10-15 09:37:23 -0500

    following pep8

--- BinaryGap.py
+++ BinaryGap.py
@@ -2,15 +2,15 @@
 # find longest substring of repeating zeros in binary of int
 def binaryGap(N):
 
-    binaryNum = str( bin(N)[2:] ) # start from 3rd index to remove binary syntax
-    binaryNum = binaryNum.lstrip('0').rstrip('0') # strip leading and trailing zeros
-    listOfZeros = binaryNum.split('1') # create a list of all of the zero strings
+    binary_num = str( bin(N)[2:] ) # start from 3rd index to remove binary syntax
+    binary_num = binary_num.lstrip('0').rstrip('0') # strip leading and trailing zeros
+    list_zeros = binary_num.split('1') # create a list of all of the zero strings
 
     max_gap = 0
-    for i in range( len(listOfZeros) ):
-        tempGap = len( listOfZeros[i] )
-        if tempGap > max_gap:
-            max_gap = tempGap
+    for i in range( len(list_zeros) ):
+        temp_gap = len( list_zeros[i] )
+        if temp_gap > max_gap:
+            max_gap = temp_gap
 
     return (max_gap)
     
