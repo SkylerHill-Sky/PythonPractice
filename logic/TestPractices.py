e38800ae043c98338bafd24c1d18ade0dc55bdc5
Skyler Hill <skylerhil@me.com>
2021-10-15 11:55:45 -0500

    fix naming conventions, add pep8

---
+++ logic/TestPractices.py
@@ -0,0 +1,14 @@
+import pytest
+import BinaryGap
+import isPrime
+import RotateArray
+import SerialNumber
+
+assert BinaryGap.binary_gap(1000000000000000000000000020) == 22
+
+assert isPrime.is_Prime(1230987623144987) == False
+
+assert RotateArray.rotate_array([1, 2, 3, 4], 2) == [3, 4, 1, 2]
+
+assert SerialNumber.serial_old('123.124.123')
+assert SerialNumber.serial_with_list_comprehension('123.124.123')
