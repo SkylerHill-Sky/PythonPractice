97dde2f411793e98cf391f4fb5b3ab272e797c46
Skyler Hill <skylerhil@me.com>
2021-10-15 12:17:52 -0500

    reorg 2

--- TestPractices.py
+++ TestPractices.py
@@ -1,14 +1,16 @@
 import pytest
-from Iterating import BinaryGap
-from Iterating import isPrime
-from Iterating import RotateArray
-from Iterating import SerialNumber
+from IteratingParsing import BinaryGap
+from IteratingParsing import isPrime
+from IteratingParsing import SerialNumber
+
+from Arrays import RotateArray
+from Arrays import OddOccurences
 
 assert BinaryGap.binary_gap(1000000000000000000000000020) == 22
 
 assert isPrime.is_Prime(1230987623144987) == False
 
-assert RotateArray.rotate_array([1, 2, 3, 4], 2) == [3, 4, 1, 2]
-
 assert SerialNumber.serial_old('123.124.123')
 assert SerialNumber.serial_with_list_comprehension('123.124.123')
+
+assert RotateArray.rotate_array([1, 2, 3, 4], 2) == [3, 4, 1, 2]
